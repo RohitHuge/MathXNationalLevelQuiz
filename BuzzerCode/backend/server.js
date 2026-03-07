15218adb44e44d0e63ebd2f634d795f6c9bd38ef
RohitHuge <rohitrhuge@gmail.com>
2026-03-07 11:52:27 +0530

    refactor- APi Assing

--- BuzzerCode/backend/server.js
+++ BuzzerCode/backend/server.js
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const ARDUINO_COM_PORT = "COM11";
-const VPS_SOCKET_URL = "http://localhost:3001";
+const VPS_SOCKET_URL = "https://api.mathxpccoer.in";
 const LOCAL_PORT = 5000;
 
 const app = express();
